f160f3a743079cbe94605c444bdfd2423f01a7b1
Geovanni <roderik317@gmail.com>
2023-01-13 16:35:53 -0600

    add some examples with sintax correct

--- test/test.js
+++ test/test.js
@@ -1,7 +1,19 @@
 const chai = require('chai');
+
+var assert = chai.assert;
 //expect
 const expect = chai.expect;
 
 //error examples
 let a = 4, b = 4;
 expect(a).to.be.equals(b);
+
+expect(a).to.equal(4);
+
+//a.should.equal(4);
+
+assert.typeOf(a, 'number');
+
+assert.equal(b, 4);
+
+
